9d5d2b57a26ba022e87e8d6e5be52f2d9e11150e
vivekstagram <therealvivek9064@gmail.com>
2017-09-27 10:53:32 -0700

    Update 9/27/17-2
    APCS WORK

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		mystery(1234);
+	}
+	
+	public static void mystery(int x)
+	{
+		System.out.print(x % 10);
+		
+		if (x / 10 != 0)
+		{
+			mystery(x / 10);
+		}
+		
+		System.out.print(x % 10);
+	}
+	
+}
