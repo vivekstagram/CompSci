11fde2ba4863b5159ed0feceb259fb43c66f77c1
Vivek Patel <therealvivek9064@gmail.com>
2017-09-30 14:02:16 -0700

    Update 9/30/17-2
    I really hate triangular prisms

--- src/GoalSheet5_3DShapeTester.java
+++ src/GoalSheet5_3DShapeTester.java
@@ -1,7 +1,6 @@
 //Vivek Patel
-//A class defining a Circle
+//A program that tests a bunch of classes representing 3D shapes
 //Goal Sheet 5 - 3D Shapes
-      
 
 public class GoalSheet5_3DShapeTester {
 
@@ -24,7 +23,7 @@ public class GoalSheet5_3DShapeTester {
 		
 		GoalSheet5_3DShape _c = new GoalSheet5_Cube(4.0f);
 		
-		System.out.println("Surface area of cube: " + _c.getSurfaceArea());
+		System.out.println("\n\nSurface area of cube: " + _c.getSurfaceArea());
 		System.out.println("Volume of cube: " + _c.getVolume());
 		
 		((GoalSheet5_Cube)_c).setSideLength(3.0f);
@@ -34,15 +33,38 @@ public class GoalSheet5_3DShapeTester {
 		
 		
 		
-		
 		GoalSheet5_3DShape _s = new GoalSheet5_Sphere(4.0f);
 		
-		System.out.println("Surface area of sphere: " + _s.getSurfaceArea());
+		System.out.println("\n\nSurface area of sphere: " + _s.getSurfaceArea());
 		System.out.println("Volume of sphere: " + _s.getVolume());
 		
 		((GoalSheet5_Sphere)_s).setRadius(3.0f);
 		
 		System.out.println("Surface area of sphere: " + _s.getSurfaceArea());
 		System.out.println("Volume of sphere: " + _s.getVolume());
+		
+		
+		
+		GoalSheet5_3DShape _cyl = new GoalSheet5_Cylinder(4.0f, 2.0f);
+		
+		System.out.println("\n\nSurface area of cylinder: " + _cyl.getSurfaceArea());
+		System.out.println("Volume of cylinder: " + _cyl.getVolume());
+		
+		((GoalSheet5_Cylinder)_cyl).setBaseRadius(3.0f);
+		
+		System.out.println("Surface area of cylinder: " + _cyl.getSurfaceArea());
+		System.out.println("Volume of cylinder: " + _cyl.getVolume());
+		
+		
+		
+		GoalSheet5_3DShape _co = new GoalSheet5_CircularCone(4.0f, 2.0f);
+		
+		System.out.println("\n\nSurface area of circular cone: " + _co.getSurfaceArea());
+		System.out.println("Volume of circular cone: " + _co.getVolume());
+		
+		((GoalSheet5_Cylinder)_co).setBaseRadius(3.0f);
+		
+		System.out.println("Surface area of circular cone: " + _co.getSurfaceArea());
+		System.out.println("Volume of circular cone: " + _co.getVolume());
 	}
 }
---
+++ src/GoalSheet5_TriangularPrism.java
@@ -0,0 +1,19 @@
+//Vivek Patel
+//A class defining a Circle
+//Goal Sheet 5 - 3D Shapes
+
+public class GoalSheet5_TriangularPrism implements GoalSheet5_3DShape {
+
+	@Override
+	public float getSurfaceArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
