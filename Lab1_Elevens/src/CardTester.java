eb3e464a39218236c171656a2e3da1aa9bfe84bc
Vivek Patel <therealvivek9064@gmail.com>
2017-10-07 17:49:29 -0700

    Worked on activity 5 a bit more

--- Lab1_Elevens/src/CardTester.java
+++ Lab1_Elevens/src/CardTester.java
@@ -17,5 +17,29 @@ public class CardTester {
 		System.out.println(_2.rank() + " " + _2.suit() + " " + _2.pointValue());
 		System.out.println(_3.rank() + " " + _3.suit() + " " + _3.pointValue());
 		
+		Card c1 = new Card("ace", "hearts", 1);
+		Card c2 = new Card("ace", "hearts", 1);
+		Card c3 = new Card("ace", "hearts", 2);
+		Card c4 = new Card("ace", "spades", 1);
+		Card c5 = new Card("king", "hearts", 1);
+		Card c6 = new Card("queen", "clubs", 3);
+		
+		
+		assert c1.rank().equals("ace") : "Wrong rank: " + c1.rank();
+		assert c1.suit().equals("hearts") : "Wrong suit: " + c1.suit();
+		assert c1.pointValue() == 1 : "Wrong point value: "
+		 + c1.pointValue();
+		assert c6.rank().equals("queen") : "Wrong rank: " + c6.rank();
+		assert c6.suit().equals("clubs") : "Wrong suit: " + c6.suit();
+		assert c6.pointValue() == 3: "Wrong point value : "
+		 + c6.pointValue();
+		
+		assert c1.toString().equals("ace of hearts (point value = 1)")
+		 : "Wrong toString: " + c1;
+		assert c6.toString().equals("queen of clubs (point value = 3)")
+		 : "Wrong toString: " + c6;
+		
+		
+		System.out.println("All tests passed!");
 	}
 }
--- Lab1_Elevens/src/DeckTester.java
+++ Lab1_Elevens/src/DeckTester.java
@@ -8,14 +8,17 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		String[] ranks = {"jack", "queen", "king"};
-		String[] suits = {"blue", "red"};
-		int[] pointValues = {11, 12, 13};
+		String[] ranks = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
+		String[] suits = {"diamonds", "spades", "clubs", "hearts"};
+		int[] pointValues = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 		
 		
 		Deck d = new Deck(ranks, suits, pointValues);
 		
-		System.out.println(d.toString());
+		System.out.println(d.toString() + "\n\n\n");
+		System.out.println(d.deal().toString() + "\n\n\n");
+		System.out.println(d.toString() + "\n\n\n");
+		
 		
 		
 	}
