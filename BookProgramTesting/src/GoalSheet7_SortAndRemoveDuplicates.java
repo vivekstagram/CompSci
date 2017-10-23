9ada417c0eaa5fd7251acea04a72a6d9ea34f264
vivekstagram <therealvivek9064@gmail.com>
2017-10-23 10:39:41 -0700

    Work

--- BookProgramTesting/src/GoalSheet7_SortAndRemoveDuplicates.java
+++ BookProgramTesting/src/GoalSheet7_SortAndRemoveDuplicates.java
@@ -2,7 +2,7 @@
  * Name: Vivek Patel
  * Date: 10/18/2017
  * Purpose: Write a method called sortAndRemoveDuplicates that accepts a list of integers as its
-			parameter and rearranges the list’s elements into sorted ascending order, as well as
+			parameter and rearranges the listï¿½s elements into sorted ascending order, as well as
 			removing all duplicate values from the list. For example, the list {7, 4, -9, 4, 15, 8, 27,
 			7, 11, -5, 32, -9, -9} would become {-9, -5, 4, 7, 8, 11, 15, 27, 32} after a call to your
 			method. Use a Set as part of your solution.
@@ -15,8 +15,10 @@ public class GoalSheet7_SortAndRemoveDuplicates
 	public static void main(String[] args)
 	{
 		List<Integer> list = new ArrayList<Integer>();
+		
 		int[] unsortedA = {7, 4, -9, 4, 15, 8, 27, 7, 11, -5, 32, -9, -9};
-		for(int i : unsortedA)//Add values to list from array
+		
+		for (int i : unsortedA)//Add values to list from array
 		{
 			list.add(i);
 		}
@@ -24,8 +26,9 @@ public class GoalSheet7_SortAndRemoveDuplicates
 		
 		list = sortAndRemoveDuplicates(list);
 		
-		System.out.println("Main List: ");//print out values
-		for(Integer i : list)
+		System.out.println("Main List: "); //print out values
+		
+		for (Integer i : list)
 		{
 			System.out.print(i + ", ");
 		}
@@ -43,7 +46,6 @@ public class GoalSheet7_SortAndRemoveDuplicates
 			//Iterate through list, add to sorted set
 			sortedWithoutDuplicates.add(i.next());
 		}
-		
 		
 		return Arrays.asList(sortedWithoutDuplicates.toArray(new Integer[0]));
 	}
