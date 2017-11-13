1ecc5f615deb40be68f3b28de476c5719bfc34d1
vivekstagram <therealvivek9064@gmail.com>
2017-11-13 15:17:39 -0800

    MergeSort

--- BookProgramTesting/src/GoalSheet9/GoalSheet9_MergeSort.java
+++ BookProgramTesting/src/GoalSheet9/GoalSheet9_MergeSort.java
@@ -7,8 +7,71 @@ package GoalSheet9;
 			efficiently sorts even a large file. (BJP Ch 13 Proj 1)
  */
 
+import java.util.*;
+import java.io.*;
 
 public class GoalSheet9_MergeSort {
 
+	public static void main(String args[])
+	{
+		List<String> theList = new ArrayList<String>();
+		List<String> cont = new ArrayList<String>();
+		
+		
+		theList.add("Amy");
+		theList.add("Connor");
+		theList.add("Vivek");
+		theList.add("Jackson");
+		theList.add("Callum");
+		
+		
+		cont = mergeSort(theList);
+		
+		System.out.println("Sorted: " + cont.toString());
+		
+	}
 	
+	public static List<String> mergeSort(List<String> theStrings)
+	{
+		System.out.println("In mergeSort we are sorting " + theStrings.toString());
+		
+		if (theStrings.size() > 1)
+		{
+			//Split into two 'halves'
+			List<String> left = theStrings.subList(0, theStrings.size() / 2);
+			List<String> right = theStrings.subList(theStrings.size() / 2, theStrings.size());
+		
+			//Recursively sort the two halves
+			mergeSort(left);
+			mergeSort(right);
+			
+			//Merge the sorted halves
+			merge(theStrings, left, right);
+		}
+		
+		return theStrings;
+	}
+	
+	public static void merge(List<String> container, List<String> left, List<String> right)
+	{
+		System.out.println("In merge we are merging " + left.toString() + " and " + right.toString());
+		
+		
+		int indexLeft = 0, indexRight = 0;
+		
+		for (int i = 0; i < container.size(); i++)
+		{
+			if ( indexRight >= right.size() || ( indexLeft < left.size() && left.get(indexLeft).compareToIgnoreCase(right.get(indexRight)) <= 0 ) )
+			{
+				container.set(i, left.get(indexLeft));
+				indexLeft++;
+			}
+			else
+			{
+				container.set(i, right.get(indexRight));
+				indexRight++;
+			}
+		}
+		
+	}
 }
