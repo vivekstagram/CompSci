5f20d70548813dd08cc71ff480d03803e836f5e4
Vivek Patel <therealvivek9064@gmail.com>
2017-11-29 19:16:59 -0800

    Studying for comp sci quiz

--- BookProgramTesting/src/Test.java
+++ BookProgramTesting/src/Test.java
@@ -3,24 +3,14 @@ public class Test {
 
 	public static void main(String[] args)
 	{
-		int [] n = {2, 10, 100, 1000, 10000}; //number of steps in each summation
-		int a = 0;
-		int b = 2;
-		double H = 0;
-		double x = 0;
-
-		for (int j = 0; j < 4; j++)
+		int i = 1, n = 100;
+		
+		for (i = 1; i < n; i++)
 		{
-		    double dX = (double)(b - a) / (double)n[j];
-		    
-		    for (int i = 1; i < n[j]; i++)
-		    {
-		        x = a + ((double)i - 1.0) * dX;
-		        H += (x - Math.pow(x, 2.0));               
-		    }
-		    
-		    double solution = H * dX;
-		    System.out.println(solution);
+			n--;
 		}
+		
+		System.out.println("i: " + i + "\nn: " + n);
+		
 	}
 }
