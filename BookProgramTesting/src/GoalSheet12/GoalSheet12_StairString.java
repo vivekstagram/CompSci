95b27fe9dd240722ae23b77fbf789d7e9b7d7a65
vivekstagram <therealvivek9064@gmail.com>
2018-02-28 10:48:07 -0800

    stairString
    push them changes

--- BookProgramTesting/src/GoalSheet12/GoalSheet12_StairString.java
+++ BookProgramTesting/src/GoalSheet12/GoalSheet12_StairString.java
@@ -2,4 +2,46 @@ package GoalSheet12;
 
 public class GoalSheet12_StairString {
 
+	
+	public static void main(String args[])
+	{
+		stairString("abcdefghijklmnopqrstuvwxyz", 20);
+	}
+	
+	
+	public static void stairString(String s, int num)
+	{
+		if (num == 0)
+		{
+			return;
+		}
+		
+		
+		int lines = 0;
+		int length = s.length() / num;
+		
+		int rem = s.length() % num;
+		if(rem == num - 1)
+		{
+			length++;
+		}
+		
+		for(int i = 0; i < s.length(); i += length)
+		{
+			for(int j = 0; j < i; j++)
+			{
+				System.out.print(" ");
+			}
+			lines++;
+			if(lines == num)
+			{
+				System.out.print(s.substring(i));
+				break;
+			}
+			else
+			{
+				System.out.println(s.substring(i, i + length));
+			}
+		}
+	}
 }
