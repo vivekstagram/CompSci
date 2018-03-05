095f63672cb3517a52b39fac4b4e527a1dccf365
Vivek Patel <therealvivek9064@gmail.com>
2018-03-04 21:34:36 -0800

    Fixed binary search
    finally.

--- BookProgramTesting/src/GoalSheet11/GoalSheet11_BJP_CH13_Ex4.java
+++ BookProgramTesting/src/GoalSheet11/GoalSheet11_BJP_CH13_Ex4.java
@@ -12,17 +12,18 @@ public class GoalSheet11_BJP_CH13_Ex4 {
 	public static void main(String args[])
 		throws FileNotFoundException
 	{
-		Scanner sc1 = new Scanner(new File("src/GoalSheet11/words.txt"));
+		Scanner sc1 = new Scanner(new File("src/GoalSheet11/wordsSorted.txt"));
 
 		List<String> theList = new ArrayList<String>();
 
 		while (sc1.hasNextLine()) {
-			String s = sc1.nextLine();
-			theList.add(s);
+			theList.add(sc1.nextLine());
 		}
 		
-		//theList.sort(String.CASE_INSENSITIVE_ORDER);
+		System.out.println("Size: " + theList.size());
 		
+		
+		theList.sort(String.CASE_INSENSITIVE_ORDER);
 		
 		Scanner sc2 = new Scanner(System.in);
 		
@@ -34,9 +35,14 @@ public class GoalSheet11_BJP_CH13_Ex4 {
 		System.out.println("What's the second word?: ");
 		String w2 = sc2.nextLine();
 		
-		
-		int ind1 = binarySearch(theList, w1, 0, theList.size() -1);
-		int ind2 = binarySearch(theList, w2, 0, theList.size() -1);
+		int ind1;
+		ind1 = binSearch(theList, w1);
+		System.out.println(ind1);
+
+		int ind2;
+		ind2 = binSearch(theList, w2);
+		System.out.println(ind2);
+
 		
 		
 		if (ind1 < 0)//Not found
@@ -64,9 +70,9 @@ public class GoalSheet11_BJP_CH13_Ex4 {
 	}
 	
 	
-	public static int binarySearch(List<String> dictionary, String target, int from, int to)
+	public static int binarySearch(List<String> dictionary, String target)
 	{
-		if ((to - from) <= 1)
+		if(dictionary.size() <= 1)
 		{
 			if(dictionary.get(0).compareToIgnoreCase(target) == 0)
 				return 0;//Word Found
@@ -74,22 +80,62 @@ public class GoalSheet11_BJP_CH13_Ex4 {
 				throw new IllegalArgumentException("Word Not Found");
 		}
 		
-		int compare = dictionary.get((to - from) / 2).compareToIgnoreCase(target);
+		int compare = dictionary.get(dictionary.size() / 2).compareToIgnoreCase(target);
 		
-		if (compare < 0)//Target comes after partition
+		if(compare < 0)//Target comes after partition
 		{
-			return binarySearch(dictionary, target, ((to - from) / 2) + 1, to);
+			List<String> theModifiedList = dictionary.subList(dictionary.size() / 2 + 1, dictionary.size());
+			
+			theModifiedList.add(dictionary.get(dictionary.size()- 1));
+			
+			return (int)Math.ceil((double)(dictionary.size() / 2)) + binarySearch(theModifiedList, target);
 			//Return number of indexes before partition, recursion with sub list
 		}
 		else if (compare > 0)
 		{
-			return binarySearch(dictionary, target, from, (to - from) / 2);
+			return binarySearch(dictionary.subList(0, dictionary.size() / 2), target);
 			//recursion with sub list
 		}
 		else
 		{
-			return (int)((to - from) / 2);
+			return (int)(dictionary.size() / 2);
 			//Index of target found
 		}
+		
+
+	}
+	
+	public static int binSearch(List<String> l, String key)
+	{
+		 int startIndex = 0;
+		 int endIndex = l.size() - 1;
+
+		 int midIndex = (endIndex + startIndex) / 2;
+		 
+		 while (!(l.get(midIndex).compareToIgnoreCase(key) == 0))
+		 {
+			 System.out.println("Middle: " + midIndex + " Start " + startIndex + " End " + endIndex);
+			 
+			 if (midIndex == 65737)
+				 System.out.println(l.get(midIndex));
+			 
+		     if (l.get(midIndex).compareToIgnoreCase(key) < 0)
+		     {
+		         startIndex = midIndex;
+		     } 
+		     else if (l.get(midIndex).compareToIgnoreCase(key) > 0)
+		     {
+		         endIndex = midIndex;
+		     }
+		     
+		     int prevMid = midIndex;
+		     
+		     midIndex = (endIndex + startIndex) / 2;
+		     
+		     if (prevMid == midIndex)
+		    	 return -9064;
+		 }
+
+		 return midIndex;
 	}
 }
