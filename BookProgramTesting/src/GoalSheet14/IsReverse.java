515676fe326625e364a0358fd2291eacbeeda763
Vivek Patel <therealvivek9064@gmail.com>
2018-04-13 17:58:09 -0700

    Finished isReverse
    Have 3 weeks to figure out what I'm gonna do for the Levenshtein distance thing and the anagrams program. Was fun using the Mavenir company MacBook Pro though. Let's hope that I can purchase one soon! #AppDevMoney

--- BookProgramTesting/src/GoalSheet14/IsReverse.java
+++ BookProgramTesting/src/GoalSheet14/IsReverse.java
@@ -5,7 +5,7 @@ public class IsReverse {
 	public static void main(String args[])
 	{
 		//should print out true
-		System.out.println(isReverse("Vivek", "Keviv"));
+		System.out.println(isReverse("stack", "KcAtS"));
 	}
 	
 	public static boolean isReverse(String s1, String s2)
@@ -13,11 +13,22 @@ public class IsReverse {
 		if (s1.length() != s2.length())
 			return false;
 		
+		//Makes comparisons a lot easier
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+		
+		//Base case, if strings are 1 character in length
 		if (s1.length() == 1 && s2.length() == 1)
-			if (s1.compareToIgnoreCase(s2) == 0)
+		{
+			if (s1.equals(s2))
 				return true;
-			
-			
+		}
+		else
+		{
+			if (s1.charAt(0) == s2.charAt(s2.length() - 1))
+				return isReverse(s1.substring(1), s2.substring(0, s2.length() - 1));
+		}
+		
 		return false;
 	}
 }
